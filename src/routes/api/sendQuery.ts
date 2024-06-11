b32fdbfbe0f0cad9cddb7863b241fa2bbfac52fb
Ryan Prendergast <rprendergast1121@gmail.com>
2024-06-10 21:27:53 -0400

    Setup openai api

--- src/routes/api/sendQuery.ts
+++ src/routes/api/sendQuery.ts
@@ -1,14 +1,27 @@
 
 import type { APIEvent } from "@solidjs/start/server"
 import { SendQueryRequest } from "~/constants/apiInterfaces"
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
 
 export async function POST(params: APIEvent) {
   const body = await params.request.json() as SendQueryRequest
   // Get the body as a string
 
   console.log(body)
+
+  const completion = await openai.chat.completions.create({
+    messages: [{"role": "user", "content": body.submission}],
+    model: "gpt-4o",
+  });
+
+  console.log(completion.choices[0]);
+
   return {
     status: 200,
-    body: { message: body.submission }
+    body: { message: completion.choices[0] }
   }
 }
